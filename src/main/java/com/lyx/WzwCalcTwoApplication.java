4a3d12ba88e52c1f167658e0998179bfca70397b
李艳兴 <liyanxing@zhongruigroup.com>
2022-09-22 11:40:49 +0800

    初始化

---
+++ src/main/java/com/lyx/WzwCalcTwoApplication.java
@@ -0,0 +1,15 @@
+package com.lyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzwCalcTwoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WzwCalcTwoApplication.class, args);
+    }
+
+}
