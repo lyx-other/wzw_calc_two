756a4fd2e9e258cd08826789a8a8c8bda6ab4c18
李艳兴 <liyanxing@zhongruigroup.com>
2022-09-23 10:43:50 +0800

    代码初步完成

--- src/main/java/com/lyx/services/FunService.java
+++ src/main/java/com/lyx/services/FunService.java
@@ -1,14 +1,18 @@
 package com.lyx.services;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.lang.Assert;
+import cn.hutool.core.util.NumberUtil;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
 import com.lyx.common.CommonResult;
+import com.lyx.entity.ExcelModel;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,7 +28,10 @@ public class FunService
     {
         // 读取excel数据
         Assert.isTrue(FileUtil.exist(filePath), "文件「{}」不存在", filePath);
-        List<BigDecimal> excelData = this.readData(filePath);
+        List<ExcelModel> excelData = this.readData(filePath, isCm);
+
+        // 进行计算
+        excelData.forEach(el -> this.calc(el, isCm));
 
         return CommonResult.successData("完成");
     }
@@ -34,43 +41,62 @@ public class FunService
      * @param filePath 文件路径
      * @return 文件数据
      */
-    private List<BigDecimal> readData(String filePath)
+    private List<ExcelModel> readData(String filePath, boolean isCm)
     {
         ExcelReader reader = ExcelUtil.getReader(filePath);
         List<BigDecimal> firstColumn = reader.readColumn(0, 1)
                                             .stream()
                                             .map(Convert::toBigDecimal)
-                                            .map(el -> el.divide(BigDecimal.valueOf(100)))
+                                            .map(el -> el.divide(BigDecimal.valueOf(100), 5, RoundingMode.HALF_EVEN))
                                             .collect(Collectors.toList());
         List<BigDecimal> secondColumn = reader.readColumn(1, 1)
                                             .stream()
                                             .map(Convert::toBigDecimal)
-                                            .map(el -> el.divide(BigDecimal.valueOf(100)))
                                             .collect(Collectors.toList());
+        if (isCm)
+        {
+            secondColumn.stream().map(el -> el.divide(BigDecimal.valueOf(100), 5, RoundingMode.HALF_DOWN));
+        }
         reader.close();
-        Assert.isTrue(firstColumn.size()==secondColumn.size(), "数据再列数目不一致");
+        Assert.isTrue(firstColumn.size()==secondColumn.size(), "数据两列数目不一致");
 
-        return secondColumn;
+        List<ExcelModel> result = CollUtil.newArrayList();
+        for (int i = 0; i <= firstColumn.size()-1; i++)
+        {
+            ExcelModel model = ExcelModel.builder().s(firstColumn.get(i)).mama(secondColumn.get(i)).build();
+            result.add(model);
+        }
+
+        return result;
     }
 
-    // /**
-    //  * 核心方法，输入一个MAMA数据，计算输出的帧.
-    //  * @param Mama 一个MAMA数据
-    //  * @param isCm 这个数据单位是不是cm.
-    //  * @return 输出的帧
-    //  */
-    // private String calc(BigDecimal mama, boolean isCm)
-    // {
-    //     BigDecimal t;
-    //     if (isCm)
-    //     {
-    //         t = mama.divide(V);
-    //     }
-    //     else
-    //     {
-    //
-    //     }
-    //
-    //     return null;
-    // }
+    /**
+     * 核心方法，输入一行excel数据，计算输出的帧.
+     * @param row 一行excel数据
+     * @param isCm 这个数据单位是不是cm.
+     * @return 输出的帧
+     */
+    private String calc(ExcelModel row, boolean isCm)
+    {
+        BigDecimal t;
+        if (isCm)
+        {
+            t = row.getMama().divide(V, 5 , RoundingMode.HALF_EVEN);
+        }
+        else
+        {
+            BigDecimal fracChild = row.getS().multiply(BigDecimal.valueOf(Math.sin(Math.toRadians(row.getMama().doubleValue()))));
+            BigDecimal fracMother = BigDecimal.valueOf(Math.sin(Math.toRadians(BigDecimal.valueOf(110).subtract(row.getMama()).doubleValue()))).multiply(V);
+            t = fracChild.divide(fracMother, 5, RoundingMode.HALF_EVEN);
+        }
+
+        // 需要采集的帧数量
+        BigDecimal count = BigDecimal.valueOf(1).divide(t, 5, RoundingMode.HALF_EVEN);
+
+        // 需要采集的帧间隔
+        BigDecimal stepSize = BigDecimal.valueOf(50).divide(count, 5, RoundingMode.HALF_EVEN);
+        int stepSizeRound = NumberUtil.round(stepSize, 0, RoundingMode.CEILING).intValue();
+
+        return null;
+    }
 }
--- src/test/java/com/lyx/Main.java
+++ src/test/java/com/lyx/Main.java
@@ -1,8 +1,13 @@
 package com.lyx;
 
+import cn.hutool.core.lang.Console;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        Console.log("打印数据：{}", Math.sin(Math.PI/3)); // TODO 删除 李艳兴
+        Console.log("打印数据：{}", Math.sqrt(3)/2); // TODO 删除 李艳兴
+        Console.log("打印数据：{}", Math.sin(Math.toRadians(60))); // TODO 删除 李艳兴
     }
 }
